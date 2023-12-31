36745fc5169b06a8b5da85e9be9f84db0959e51f
neymarsabin <reddevil.sabin@gmail.com>
2023-12-31 14:41:53 +0545

    add port configuration, start server based on that port config

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -21,8 +22,8 @@ import (
 )
 
 func main() {
-	// start with initializing a few commands
 	config := &Configuration{}
+	// eg: sack start --port=8080
 	cmds := []acmd.Command{
 		{
 			Name:        "ping",
@@ -33,12 +34,99 @@ func main() {
 			},
 		},
 		{
-			Name:        "port",
+			Name:        "start",
 			Description: "set port of the application",
 			ExecFunc: func(ctx context.Context, args []string) error {
-				config.port = 6379
-				fmt.Println("The port is set for configuration: ", config.port)
-				return nil
+				fs := flag.NewFlagSet("port", flag.ContinueOnError)
+				portFlag := fs.Int("port", 6379, "Port number to use")
+
+				if err := fs.Parse(args); err != nil {
+					return err
+				}
+
+				config.port = *portFlag
+				fmt.Println("Server is listening.....")
+				portConfiguration := fmt.Sprintf(":%d", config.port)
+				listener, err := net.Listen("tcp", portConfiguration)
+				if err != nil {
+					fmt.Println("Error while starting listener: ", err)
+					return err
+				}
+
+				// start with persistance file
+				persistance, err := NewPersistance("db.sack")
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				defer persistance.Close()
+
+				// read logs from file and save it in memory
+				// does not have the Read method without using the ~file.Read~ option
+				persistance.Read(func(value Value) {
+					command := strings.ToUpper(value.array[0].bulk)
+					args := value.array[1:]
+
+					handler, ok := Handlers[command]
+					if !ok {
+						fmt.Println("Invalid Command: ", command)
+						return
+					}
+
+					handler(args)
+				})
+
+				// start listening on the specified port
+				conn, err := listener.Accept()
+				if err != nil {
+					fmt.Println("Error while listening: ", err)
+					return err
+				}
+
+				// starting the server
+				fmt.Println("Starting the server::: ")
+
+				defer conn.Close()
+
+				for {
+					resp := NewResp(conn)
+					value, err := resp.Read()
+
+					if err != nil {
+						fmt.Println(err)
+						return err
+					}
+
+					if value.typ != "array" {
+						fmt.Println("Invalid request, expected array")
+						continue
+					}
+
+					if len(value.array) == 0 {
+						fmt.Println("Invalid request, expected array length > 0")
+						continue
+					}
+
+					command := strings.ToUpper(value.array[0].bulk)
+					args := value.array[1:]
+
+					writer := NewWriter(conn)
+					handler, ok := Handlers[command]
+
+					if !ok {
+						fmt.Println("Invalid command: ", command)
+						writer.Write(Value{typ: "string", str: ""})
+						continue
+					}
+
+					if command == "SET" || command == "HSET" {
+						persistance.Write(value)
+					}
+
+					result := handler(args)
+					writer.Write(result)
+					return nil
+				}
 			},
 		},
 	}
@@ -51,87 +139,5 @@ func main() {
 	if err := app.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
-	}
-
-	// create a new server to listen to port 6379
-	fmt.Println("Listening on port :6379")
-	listener, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Error while starting listener: ", err)
-		return
-	}
-
-	// start with persistance file
-	persistance, err := NewPersistance("db.sack")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer persistance.Close()
-
-	// read logs from file and save it in memory
-	// does not have the Read method without using the ~file.Read~ option
-	persistance.Read(func(value Value) {
-		command := strings.ToUpper(value.array[0].bulk)
-		args := value.array[1:]
-
-		handler, ok := Handlers[command]
-		if !ok {
-			fmt.Println("Invalid Command: ", command)
-			return
-		}
-
-		handler(args)
-	})
-
-	// start listening on the specified port
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error while listening: ", err)
-		return
-	}
-
-	// starting the server
-	fmt.Println("Starting the server::: ")
-
-	defer conn.Close()
-
-	for {
-		resp := NewResp(conn)
-		value, err := resp.Read()
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		if value.typ != "array" {
-			fmt.Println("Invalid request, expected array")
-			continue
-		}
-
-		if len(value.array) == 0 {
-			fmt.Println("Invalid request, expected array length > 0")
-			continue
-		}
-
-		command := strings.ToUpper(value.array[0].bulk)
-		args := value.array[1:]
-
-		writer := NewWriter(conn)
-		handler, ok := Handlers[command]
-
-		if !ok {
-			fmt.Println("Invalid command: ", command)
-			writer.Write(Value{typ: "string", str: ""})
-			continue
-		}
-
-		if command == "SET" || command == "HSET" {
-			persistance.Write(value)
-		}
-
-		result := handler(args)
-		writer.Write(result)
 	}
 }
